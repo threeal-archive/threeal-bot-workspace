37c758924fb230f1500110d0deeea50b1ff87d2c
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-02-28 21:16:02 +0700

    set *_BRANCH env value as optional

--- start.bash
+++ start.bash
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 # Prepare Threeal bot from specific repo and branch
-if [ ! -z $BOT_REPO ] && [ ! -z $BOT_REPO_BRANCH ]; then
+if [ ! -z $BOT_REPO ]; then
   rm -rf /app/threeal-bot || exit $?
-  git clone -b $BOT_REPO_BRANCH $BOT_REPO /app/threeal-bot || exit $?
+  if [ -z $BOT_REPO_BRANCH ]; then
+    git clone $BOT_REPO /app/threeal-bot || exit $?
+  else
+    git clone -b $BOT_REPO_BRANCH $BOT_REPO /app/threeal-bot || exit $?
+  fi
   cd /app/threeal-bot && yarn install || exit $?
 fi
 
 # Prepare Threeal bot NLU from specific repo and branch
-if [ ! -z $NLU_REPO ] && [ ! -z $NLU_REPO_BRANCH ]; then
+if [ ! -z $NLU_REPO ]; then
   rm -rf /app/threeal-bot-nlu || exit $?
-  git clone -b $NLU_REPO_BRANCH $NLU_REPO /app/threeal-bot || exit $?
+  if [ -z $NLU_REPO_BRANCH ]; then
+    git clone $NLU_REPO /app/threeal-bot || exit $?
+  else
+    git clone -b $NLU_REPO_BRANCH $NLU_REPO /app/threeal-bot || exit $?
+  fi
   cd /app/threeal-bot-nlu && rasa train || exit $?
 fi
 
