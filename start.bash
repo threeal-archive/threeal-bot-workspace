da197f06e228a6c01872ff6d2ff4eba1064dfd1f
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-02-28 21:24:36 +0700

    minor typo fix

--- start.bash
+++ start.bash
@@ -15,9 +15,9 @@ fi
 if [ ! -z $NLU_REPO ]; then
   rm -rf /app/threeal-bot-nlu || exit $?
   if [ -z $NLU_REPO_BRANCH ]; then
-    git clone $NLU_REPO /app/threeal-bot || exit $?
+    git clone $NLU_REPO /app/threeal-bot-nlu || exit $?
   else
-    git clone -b $NLU_REPO_BRANCH $NLU_REPO /app/threeal-bot || exit $?
+    git clone -b $NLU_REPO_BRANCH $NLU_REPO /app/threeal-bot-nlu || exit $?
   fi
   cd /app/threeal-bot-nlu && rasa train || exit $?
 fi
