6839547610b07b5a6eebec019cd6cc67d763f218
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-02-28 21:55:43 +0700

    makes only pull if the same repo

--- start.bash
+++ start.bash
@@ -1,25 +1,38 @@
 #!/bin/bash
 
-# Prepare Threeal bot from specific repo and branch
-if [ ! -z $BOT_REPO ]; then
-  rm -rf /app/threeal-bot || exit $?
-  if [ -z $BOT_REPO_BRANCH ]; then
-    git clone $BOT_REPO /app/threeal-bot || exit $?
+check_repo () {
+  REPO="$1"
+  BRANCH="$2"
+  LOCATION="$3"
+
+  # Check if is the same repo
+  if cd "$LOCATION" && [ -f '.git' ] \
+    && [ "$REPO" = "$(&& git remote get-url origin)" ]; then
+    git pull origin HEAD
   else
-    git clone -b $BOT_REPO_BRANCH $BOT_REPO /app/threeal-bot || exit $?
+    cd / && rm -rf $LOCATION || return $?
+
+    # Clone with or without specific branch
+    if [ -z "$BRANCH" ]; then
+      git clone "$REPO" "$LOCATION" || return $?
+    else
+      git clone -b "$BRANCH" "$REPO" "$LOCATION" || return $?
+    fi
   fi
-  cd /app/threeal-bot && yarn install || exit $?
+}
+
+# Prepare Threeal bot from specific repo and branch
+if [ ! -z "$BOT_REPO" ]; then
+  if check_repo "$BOT_REPO" "$BOT_REPO_BRANCH" /app/threeal-bot || exit $?; then
+    cd /app/threeal-bot && yarn install || exit $?
+  fi
 fi
 
 # Prepare Threeal bot NLU from specific repo and branch
-if [ ! -z $NLU_REPO ]; then
-  rm -rf /app/threeal-bot-nlu || exit $?
-  if [ -z $NLU_REPO_BRANCH ]; then
-    git clone $NLU_REPO /app/threeal-bot-nlu || exit $?
-  else
-    git clone -b $NLU_REPO_BRANCH $NLU_REPO /app/threeal-bot-nlu || exit $?
+if [ ! -z "$NLU_REPO" ]; then
+  if check_repo "$NLU_REPO" "$NLU_REPO_BRANCH" /app/threeal-bot-nlu || exit $?; then
+    cd /app/threeal-bot-nlu && rasa train || exit $?
   fi
-  cd /app/threeal-bot-nlu && rasa train || exit $?
 fi
 
 supervisord
